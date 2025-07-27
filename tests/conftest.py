c2712d1951a512358690cb0beb7caa4c91c48a77
Viacheslav Suchkov <iexploudi@gmail.com>
2025-07-27 22:03:33 +0400

    add some fixture and check log in

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+@pytest.fixture(scope='session')
+def setup_browser():
+    pass
+
+@pytest.fixture(scope='session')
+def log_in():
+    pass
---
+++ tests/test_be_log_in.py
@@ -0,0 +1,2 @@
+def test_should_be_log_in(setup_browser):
+    pass
