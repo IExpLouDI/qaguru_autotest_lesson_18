18df1f390a209319a3f07ce89b3e5e77c69b8120
Viacheslav Suchkov <iexploudi@gmail.com>
2025-07-31 22:23:13 +0400

    update project stay stable version

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ import requests as rq
 
 @pytest.fixture(scope='session')
 def get_cookie():
+
     request_body = {'Email': 'demoqa@test.su',
                     'Password': 'qwe123',
                     'RememberMe': False}
@@ -14,7 +15,7 @@ def get_cookie():
     return response.cookies.get('NOPCOMMERCE.AUTH')
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 def setup_browser(get_cookie):
 
     browser.open('https://demowebshop.tricentis.com')
@@ -27,21 +28,29 @@ def setup_browser(get_cookie):
 
 @pytest.fixture(scope='session')
 def setup_session(get_cookie):
-    session = rq.session()
-    session.cookies.set('NOPCOMMERCE.AUTH',get_cookie , domain='demowebshop.tricentis.com')
-    return session
-
-@pytest.fixture()
-def add_item(setup_session):
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'Accept': '*/*',
         'Sec-Fetch-Site': 'same-origin',
         'Accept-Language': 'ru',
     }
-    payload = ("product_attribute_72_5_18=53&product_attribute_72_6_19=54&product_attribute_72_3_20=57"
-                         "&addtocart_72.EnteredQuantity=1")
-    setup_session.post(url='https://demowebshop.tricentis.com/addproducttocart/details/72/1',headers=headers, data=payload)
+    session = rq.session()
+    session.cookies.set('NOPCOMMERCE.AUTH',get_cookie , domain='demowebshop.tricentis.com')
+    session.headers=headers
+    return session
 
-    return True
+@pytest.fixture(params=['1', '10', '10000'])
+def add_item(setup_session, request):
 
+    payload = (f"product_attribute_72_5_18=53&product_attribute_72_6_19=54&product_attribute_72_3_20=57"
+                         f"&addtocart_72.EnteredQuantity={request.param}")
+    setup_session.post(url='https://demowebshop.tricentis.com/addproducttocart/details/72/1', data=payload)
+
+    yield request.param
+
+
+@pytest.fixture()
+def clean():
+    yield
+    browser.element('.qty-input').clear().type(0)
+    browser.element("[name='updatecart']").click()
--- tests/test_demowebshop.py
+++ tests/test_demowebshop.py
@@ -1,4 +1,4 @@
-from selene import have, browser
+from selene import have, browser, be
 
 
 def test_should_be_log_in(setup_browser):
@@ -6,6 +6,7 @@ def test_should_be_log_in(setup_browser):
     setup_browser.element('.account').should(have.text('demoqa@test.su'))
 
 
-def test_check_shopping_cart(setup_browser:browser, add_item):
+def test_check_shopping_cart(setup_browser:browser, add_item, clean):
     setup_browser.open('https://demowebshop.tricentis.com/cart')
     setup_browser.element('.product-name').should(have.text('Build your own cheap computer'))
+    setup_browser.element(f".qty input[value='{add_item}']").should(be.present_in_dom)
